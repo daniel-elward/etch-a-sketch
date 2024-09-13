65390e465a165fcdf3ed5a567b168da1dca35f69
LR18 <daniel.elward@hotmail.com>
2024-09-13 20:25:36 -0300

    added logic to change element color.
    BUG: current iteration changes the divs in static
    order.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+//populate the grid
 for (let i = 1; i <= 256; i++) {
     let wrapper = document.querySelector("#wrapper");
     let grid = document.createElement("div");
     grid.classList.add('grid');
     wrapper.appendChild(grid);
 };
+
+//mouse hover to change color
+let gridSelection = document.querySelector(".grid");
+
+document.addEventListener("mouseover", () => {
+    document.querySelector(".grid").classList.add("changeColor");
+    document.querySelector(".grid").classList.remove("grid");
+});
