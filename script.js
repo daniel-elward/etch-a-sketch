f620d67cca5c69db7ae5f64c123925d6f995bbcd
LR18 <daniel.elward@hotmail.com>
2024-09-13 19:59:51 -0300

    added 16x16 grid using flexbox and DOM manipulation

---
+++ script.js
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 256; i++) {
+    let wrapper = document.querySelector("#wrapper");
+    let grid = document.createElement("div");
+    grid.classList.add('grid');
+    wrapper.appendChild(grid);
+};
