b93638fa0f9b77428cb309728d74a91c6269e4f3
LR18 <daniel.elward@hotmail.com>
2024-09-17 21:17:36 -0300

    grid selection
    added logic to get users grid selection

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
-//populate the grid
-for (let i = 1; i <= 256; i++) {
-    let wrapper = document.querySelector("#wrapper");
-    let grid = document.createElement("div");
-    grid.classList.add('grid');
-    wrapper.appendChild(grid);
-};
+//get users grid selection and populate the grid
+function gridSelection(){
+    const grids = document.getElementsByName('gridChoice');
+    const textOut = document.getElementById('testOut');
+    let userChoice;
 
-//mouse hover to change color
+    for(let i = 0; i < grids.length; i++){
+        
+        if(grids[i].checked) {
+            textOut.innerHTML = grids[i].value; //testing log
+            userChoice = grids[i].value;
+        }
+    }
 
-let gridSelection = document.getElementsByClassName("grid");
+    //testing log
+    console.log(`from inside gridSelection function ${userChoice}`)
+}
 
-document.addEventListener("mouseover", function (event){
-    event.target.classList.add("changeColor");
-    event.target.classList.remove("grid");
-}); 
+const submit = document.getElementById('gridSelectionBtn');
+submit.addEventListener("click", gridSelection);
 
 
 /*
+let radioGridSelection;
 
-  document.querySelector(".grid");
+//define button click
+const gridSetBtn = document.getElementById("gridSelectionBtn");
+gridSetBtn.addEventListener("click", function (){
 
-    document.querySelector(".grid").classList.add("changeColor");
-    document.querySelector(".grid").classList.remove("grid");
+    //radio button selection
+    radioGridSelection = document.querySelector('input[name="gridChoice"]:checked').value;
 
+    return radioGridSelection;
+});
+
+console.log(radioGridSelection)
+
+
+//populate the grid
+//grid sizes
+// 16x16 = 256 (35px boxes) 32x32 = 1024 (17.5px boxes) 64x64 = 4096 (8.75px boxes)
+
+if (radioGridSelection === 256) {
+    console.log(radioGridSelection)
+    //let gridWidthAndHeight = document.querySelector('div[class="grid"]')
+    
+    //element.style.width = "35px";
+    //element.style.height = "35px";
+
+} else if (radioGridSelection === 1024) {
+    console.log(radioGridSelection)
+
+    gridWidthAndHeight = document.querySelector('div[class="grid"]')
+    
+    gridWidthAndHeight.style.width = "17.5px";
+    gridWidthAndHeight.style.height = "17.5px";
+  
+}
+
+for (let i = 1; i <= radioGridSelection; i++) {
+    let wrapper = document.getElementById("gameWrapper");
+    let grid = document.createElement("div");
+    grid.classList.add("grid");
+    wrapper.appendChild(grid);
+};
 */
